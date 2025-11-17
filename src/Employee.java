e3293edaa83b32a430c2b50dc8b42d9750eb7113
wuhu-fan <201915537+wuhu-fan@users.noreply.github.com>
2025-11-17 21:19:57 +0800

    Eployee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,32 @@
-public class Employee {
+public class Employee extends Person {
+    private String employeeId;
+    private String role; 
 
+    public Employee() {
+        super(); // calls Person's default constructor
+        this.employeeId = "";
+        this.role = "";
+    }
+
+    public Employee(String name, int age, String email, String employeeId, String role) {
+        super(name, age, email);
+        this.employeeId = employeeId;
+        this.role = role;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
