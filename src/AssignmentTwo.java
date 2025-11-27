66895a460b96e844ebcce646faad395b031e433d
wuhu-fan <201915537+wuhu-fan@users.noreply.github.com>
2025-11-27 17:32:24 +0800

    update AssignmentTwo for rideMax

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -10,13 +10,13 @@ public class AssignmentTwo {
     public void partThree() {
         // Create a new Ride object
         Employee operator = new Employee("John", 30, "male", "E001", "Operator");
-        Ride rollerCoaster = new Ride("Roller Coaster", 140, operator);
+        Ride rollerCoaster = new Ride("Roller Coaster", 140, operator,3);
 
-        Visitor visitor1 = new Visitor("Alice", 25, "male","E001", "Single ticket", 150);
-        Visitor visitor2 = new Visitor("Bob", 22, "female", "E002","Single ticket", 160);
-        Visitor visitor3 = new Visitor("Charlie", 28, "male","E003", "VIP", 145);
-        Visitor visitor4 = new Visitor("David", 24, "female", "E004","Annual pass", 170);
-        Visitor visitor5 = new Visitor("Eve", 29, "male","E005", "Annual pass", 155);
+        Visitor visitor1 = new Visitor("Alice", 25, "male", "E001", "Single ticket", 150);
+        Visitor visitor2 = new Visitor("Bob", 22, "female", "E002", "Single ticket", 160);
+        Visitor visitor3 = new Visitor("Charlie", 28, "male", "E003", "VIP", 145);
+        Visitor visitor4 = new Visitor("David", 24, "female", "E004", "Annual pass", 170);
+        Visitor visitor5 = new Visitor("Eve", 29, "male", "E005", "Annual pass", 155);
 
         rollerCoaster.addVisitorToQueue(visitor1);
         rollerCoaster.addVisitorToQueue(visitor2);
@@ -32,15 +32,15 @@ public class AssignmentTwo {
     }
 
     public void partFourA() {
-         // Create a new Ride object
+        // Create a new Ride object
         Employee operator = new Employee("John", 30, "male", "E001", "Operator");
-        Ride ferrisWheel = new Ride("Ferris Wheel", 140, operator);
+        Ride ferrisWheel = new Ride("Ferris Wheel", 140, operator,4);
 
-        Visitor visitor1 = new Visitor("Alice", 25, "male","E001", "Single ticket", 150);
-        Visitor visitor2 = new Visitor("Bob", 22, "female", "E002","Single ticket", 160);
-        Visitor visitor3 = new Visitor("Charlie", 28, "male","E003", "VIP", 145);
-        Visitor visitor4 = new Visitor("David", 24, "female", "E004","Annual pass", 170);
-        Visitor visitor5 = new Visitor("Eve", 29, "male","E005", "Annual pass", 155);
+        Visitor visitor1 = new Visitor("Alice", 25, "male", "E001", "Single ticket", 150);
+        Visitor visitor2 = new Visitor("Bob", 22, "female", "E002", "Single ticket", 160);
+        Visitor visitor3 = new Visitor("Charlie", 28, "male", "E003", "VIP", 145);
+        Visitor visitor4 = new Visitor("David", 24, "female", "E004", "Annual pass", 170);
+        Visitor visitor5 = new Visitor("Eve", 29, "male", "E005", "Annual pass", 155);
 
         ferrisWheel.addVisitorToHistory(visitor1);
         ferrisWheel.addVisitorToHistory(visitor2);
@@ -50,30 +50,28 @@ public class AssignmentTwo {
 
         boolean isIN = ferrisWheel.checkVisitorFromHistory(visitor5);
         if (isIN) {
-            System.out.println(visitor5.getName()+" is in the ride history.");
-        }else{
-            System.out.println(visitor5.getName()+" is not in the ride history.");
+            System.out.println(visitor5.getName() + " is in the ride history.");
+        } else {
+            System.out.println(visitor5.getName() + " is not in the ride history.");
         }
 
-        int num =  ferrisWheel.numberOfVisitors();
-        System.out.println("The number of "+ferrisWheel.getRideName()+": "+num);
+        int num = ferrisWheel.numberOfVisitors();
+        System.out.println("The number of " + ferrisWheel.getRideName() + ": " + num);
 
         ferrisWheel.printRideHistory();
 
-
     }
 
-
     public void partFourB() {
-         // Create a new Ride object
+        // Create a new Ride object
         Employee operator = new Employee("John", 30, "male", "E001", "Operator");
-        Ride thunderStorm = new Ride("Thunder Storm", 140, operator);
+        Ride thunderStorm = new Ride("Thunder Storm", 140, operator,4);
 
-        Visitor visitor1 = new Visitor("Alice", 25, "male","E001", "Single ticket", 150);
-        Visitor visitor2 = new Visitor("Jacket", 22, "female", "E002","Single ticket", 160);
-        Visitor visitor3 = new Visitor("Alice", 28, "male","E003", "VIP", 145);
-        Visitor visitor4 = new Visitor("Andy", 24, "female", "E004","Annual pass", 170);
-        Visitor visitor5 = new Visitor("Macket", 29, "male","E005", "Annual pass", 155);
+        Visitor visitor1 = new Visitor("Alice", 25, "male", "E001", "Single ticket", 150);
+        Visitor visitor2 = new Visitor("Jacket", 22, "female", "E002", "Single ticket", 160);
+        Visitor visitor3 = new Visitor("Alice", 28, "male", "E003", "VIP", 145);
+        Visitor visitor4 = new Visitor("Andy", 24, "female", "E004", "Annual pass", 170);
+        Visitor visitor5 = new Visitor("Macket", 29, "male", "E005", "Annual pass", 155);
 
         thunderStorm.addVisitorToHistory(visitor1);
         thunderStorm.addVisitorToHistory(visitor5);
@@ -87,8 +85,14 @@ public class AssignmentTwo {
         thunderStorm.printRideHistory();
 
     }
-    public void partFive() {}
-    public void partSix() {}
-    public void partSeven() {}
+
+    public void partFive() {
+    }
+
+    public void partSix() {
+    }
+
+    public void partSeven() {
+    }
 
 }
